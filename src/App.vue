ed36d6dd0b7a5c1627bd60cea1a2b90e2f29041e
Evie <evie.skinner@e-days.com>
2020-09-22 17:25:20 +0100

    Added button in v-for that will display each lang available. On click, it translates the page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <button v-for="language in languages" :key="language.title" @click="changeLocale(language.language)" class="language-button">
+      {{ language.title }}
+    </button>
     <HelloI18n />
   </div>
 </template>
@@ -9,17 +12,34 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 import HelloI18n from './components/HelloI18n.vue';
+import SelectLanguageButton from './components/SelectLanguageButton.vue';
 
 @Component({
   components: {
     HelloWorld,
-    HelloI18n
+    HelloI18n,
+    SelectLanguageButton
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+  get languages(): Array<object> {
+    const languages = [
+      { language: 'en', title: 'English'  },
+      { language: 'fr', title: 'Francais'  },
+      { language: 'ar', title: 'عربية'  }
+    ]
+
+    return languages;
+  }
+
+  public changeLocale(locale: string): void {
+    this.$i18n.locale = locale;
+  }
+}
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,4 +48,11 @@ export default class App extends Vue {}
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.language-button {
+        background: rgb(15, 39, 105);
+        color: springgreen;
+        font-weight: 600;
+        padding: 2%;
+    }
 </style>
---
+++ src/components/SelectLanguageButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="language-button"></button>    
+</template>
+
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class SelectLanguageButton extends Vue{
+
+    @Prop()
+    language!: string;
+}
+</script>
+
+
+<style scoped>
+    .language-button {
+        background: tomato;
+        color: springgreen;
+        font-weight: 600;
+    }
+</style>
