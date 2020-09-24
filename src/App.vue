5d0ecd484cfdaed056c07c203555c2b774c586d3
Evie <evie.skinner@e-days.com>
2020-09-24 10:53:53 +0100

    Added CheeseSection component and improved styles. Made the app cheese themed of course. Added Navbar component. Added more translations

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <button v-for="language in languages" :key="language.title" @click="changeLocale(language.language)" class="language-button">
-      {{ language.title }}
-    </button>
-    <HelloI18n />
+    <Navbar />
+    <CheeseSection msg="Cheese Days"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-import HelloI18n from './components/HelloI18n.vue';
-import SelectLanguageButton from './components/SelectLanguageButton.vue';
+import CheeseSection from './components/CheeseSection.vue';
+import Navbar from './components/Navbar.vue';
 
 @Component({
   components: {
-    HelloWorld,
-    HelloI18n,
-    SelectLanguageButton
+    CheeseSection,
+    Navbar
   },
 })
-export default class App extends Vue {
-
-  get languages(): Array<object> {
-    const languages = [
-      { language: 'en', title: 'English'  },
-      { language: 'fr', title: 'Francais'  },
-      { language: 'ar', title: 'عربية'  }
-    ]
-
-    return languages;
-  }
-
-  public changeLocale(locale: string): void {
-    this.$i18n.locale = locale;
-  }
-}
+export default class App extends Vue {}
 </script>
 
 <style scoped>
@@ -46,13 +26,6 @@ export default class App extends Vue {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #ffeabf;
 }
-
-.language-button {
-        background: rgb(15, 39, 105);
-        color: springgreen;
-        font-weight: 600;
-        padding: 2%;
-    }
 </style>
---
+++ src/components/CheeseSection.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="cheese-section">
+    <img alt="Photo of a cheese" class="cheese-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Mahon_Cheese.JPG/1200px-Mahon_Cheese.JPG">
+    <!-- for the real thing we should use component interpolation with the i18n functional component to avoid XSS attacks. Not sure I understand how to do this yet -->
+    <h1>{{ $t('welcome') }}</h1>
+    <button v-for="language in languages" :key="language.title" @click="changeLocale(language.language)" class="language-button">
+      {{ language.title }}
+    </button>
+    <h2>{{ $t('book-a-cheese') }}</h2>
+    <!-- for R to L languages, the plural must be on the left -->
+    <h1>{{ $tc('insight', 2) }}</h1>
+    <HelloI18n />
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import HelloI18n from './HelloI18n.vue';
+
+
+@Component({
+  components: {
+    HelloI18n
+  },
+})
+export default class CheeseSection extends Vue {
+  @Prop() private msg!: string;
+
+  get languages(): Array<object> {
+    const languages = [
+      { language: 'en', title: 'English' },
+      { language: 'fr', title: 'Français' },
+      { language: 'ar', title: 'عربية' }
+    ]
+
+    return languages;
+  }
+
+  public changeLocale(locale: string): void {
+    this.$i18n.locale = locale;
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+  color: #22211f;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #5a2605;
+}
+
+.cheese-section {
+  background: #ffeabf;
+  padding-top: 80px;
+}
+.cheese-image {
+  max-width: 15%;
+  max-height: 15%;
+}
+.language-button {
+  background:  #2c3e50;
+  color: #fcc455;
+  font-weight: 600;
+  padding: 0.3%;
+  border-radius: 20%;
+}
+</style>
